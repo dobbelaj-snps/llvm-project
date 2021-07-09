5a9d32bd81bae6913d3d9514afd4c0a6d2f758e8
Jeroen Dobbelaere <jeroen.dobbelaere@synopsys.com>
2021-07-09 08:35:57 +0200

    [MemoryLocation] Learn about ptr_provenance.

--- llvm/lib/Analysis/MemoryLocation.cpp
+++ llvm/lib/Analysis/MemoryLocation.cpp
@@ -38,7 +38,7 @@ MemoryLocation MemoryLocation::get(const LoadInst *LI) {
   const auto &DL = LI->getModule()->getDataLayout();
 
   return MemoryLocation(
-      LI->getPointerOperand(),
+      LI->getPointerOperand(), LI->getPtrProvenance(),
       LocationSize::precise(DL.getTypeStoreSize(LI->getType())),
       LI->getAAMetadata());
 }
@@ -46,7 +46,7 @@ MemoryLocation MemoryLocation::get(const LoadInst *LI) {
 MemoryLocation MemoryLocation::get(const StoreInst *SI) {
   const auto &DL = SI->getModule()->getDataLayout();
 
-  return MemoryLocation(SI->getPointerOperand(),
+  return MemoryLocation(SI->getPointerOperand(), SI->getPtrProvenance(),
                         LocationSize::precise(DL.getTypeStoreSize(
                             SI->getValueOperand()->getType())),
                         SI->getAAMetadata());
